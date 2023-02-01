c3b343543a3f4257c2081c2d77a56d3dac2d1947
J.D <J.D.@mail.ru>
2023-02-01 21:27:00 +0300

    Add task

---
+++ homework_13/blog/database/db_init.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+database = sqlite3.connect("../database.db")
+
+with open("posts.sql") as f:
+    database.executescript(f.read())
+
+
+cir = database.cursor()
+
+cir.execute("INSERT INTO posts (author,title,content) VALUES(?,?,?)",
+            ("IVAN","fIRST POST!","iT IS WORKED"))
+
+
+cir.execute("INSERT INTO posts (author,title,content) VALUES(?,?,?)",
+            ("IVAN","/SECOND POST!","hello evryone"))
+
+database.commit()
+database.close()
